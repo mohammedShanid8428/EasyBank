1570a77176513db509e2fc966a3260062ae9483e
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-06-24 18:27:12 +0000

    index.js: navigation bar links updated and active

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const navLinks=document.querySelectorAll("nav a")
-navLinks.forEach(item=>{
-  item.addEventListener("click",()=>{
-    navLinks.forEach(e=>e.classList.remove("active"));
+// #function for navLinks
+
+const navLinks = document.querySelectorAll("nav a")
+navLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    navLinks.forEach(e => e.classList.remove("active"));
     item.classList.add("active")
   })
 })
