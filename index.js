f986f8e0d70a21fac82304ff340b2053749ecf32
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-06-25 07:39:27 +0000

    burger: Add burger toggle styles and functionality

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const navLinks = document.querySelectorAll("nav a")
 navLinks.forEach(item => {
   item.addEventListener("click", () => {
     navLinks.forEach(e => e.classList.remove("active"));
-    item.classList.add("active")
+    item.classList.add("active");
+    nav.classList.remove("active")
   })
 })
 
- function toggleMenu() {
-      document.querySelector("nav").classList.toggle("active");
-    }
+const nav = document.querySelector("nav");
+const burger = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+const navLink = document.querySelectorAll("nav a");
+
+function toggleMenu() {
+  nav.classList.toggle("active");
+  burger.classList.toggle("open");
+  overlay.classList.toggle("active");
+}
+
+navLink.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+    burger.classList.remove("open");
+    overlay.classList.remove("active");
+  });
+});
